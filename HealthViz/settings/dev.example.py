94cd1909a0634169dba1ebdd8a014408cd2800b2
lucaluca <jcgiuffri@gmail.com>
2016-07-11 21:42:54 -0500

    Fix bug in dev.example.py

--- HealthViz/settings/dev.example.py
+++ HealthViz/settings/dev.example.py
@@ -12,7 +12,7 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
